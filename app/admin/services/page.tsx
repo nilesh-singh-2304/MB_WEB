dc5609c8dc7841b0dc95e3203549df23b990cc80
nilesh-singh-2304 <shyaam2304@gmail.com>
2024-07-29 00:34:10 +0530

    removed all get requests

--- app/admin/services/page.tsx
+++ app/admin/services/page.tsx
@@ -12,134 +12,135 @@ import { SiGmail } from "react-icons/si";
 import { MdDelete } from "react-icons/md";
 import { FaEdit } from "react-icons/fa";
 const Comps = async() => {
-    let coms = await axios.get(`${process.env.NEXT_PUBLIC_HOST}/api/serv`)
-    let Data = coms.data
-    let comps = Data.comps
-    console.log(comps) 
+    // let coms = await axios.get(`${process.env.NEXT_PUBLIC_HOST}/api/serv`)
+    // let Data = coms.data
+    // let comps = Data.comps
+    // console.log(comps) 
   return (
-    <div>
+//     <div>
      
-     <Remfot/>
-     <div className="p-4 sm:ml-64">
-   <div className="p-4 border-2 border-gray-200 border-dashed rounded-lg dark:border-gray-700 mt-14">
-     <p className='text-2xl text-center font-bold'>Admin Competition Section</p>
-     <div className='flex mt-8'>
-        <p className='w-2/3'>Here you an check and control all our competitions <br /> You can add , delete and edit the competitions</p>
-        <AddServ/>
-     </div>
+//      <Remfot/>
+//      <div className="p-4 sm:ml-64">
+//    <div className="p-4 border-2 border-gray-200 border-dashed rounded-lg dark:border-gray-700 mt-14">
+//      <p className='text-2xl text-center font-bold'>Admin Competition Section</p>
+//      <div className='flex mt-8'>
+//         <p className='w-2/3'>Here you an check and control all our competitions <br /> You can add , delete and edit the competitions</p>
+//         <AddServ/>
+//      </div>
 
      
 
-<div className="relative overflow-x-auto shadow-md sm:rounded-lg mt-8">
-    <table className="w-full text-sm text-left rtl:text-right text-gray-500 dark:text-gray-400">
-        <thead className="text-xs text-gray-700 uppercase bg-gray-50 dark:bg-gray-700 dark:text-gray-400">
-            <tr>
+// <div className="relative overflow-x-auto shadow-md sm:rounded-lg mt-8">
+//     <table className="w-full text-sm text-left rtl:text-right text-gray-500 dark:text-gray-400">
+//         <thead className="text-xs text-gray-700 uppercase bg-gray-50 dark:bg-gray-700 dark:text-gray-400">
+//             <tr>
                 
-                <th scope="col" className="px-6 py-3">
-                    Name
-                </th>
-                <th scope="col" className="px-6 py-3">
-                    Image
-                </th>
-                <th scope="col" className="px-6 py-3">
-                    Whatsapp URL
-                </th>
-                <th scope="col" className="px-6 py-3">
-                    Gmail URL
-                </th>
-                <th scope="col" className="px-6 py-3">
-                    Description
-                </th>
-                <th scope="col" className="px-6 py-3">
-                    Charges
-                </th>
-                <th scope="col" className="px-6 py-3">
-                    Status
-                </th>
-                <th scope="col" className="px-6 py-3">
-                    Action
-                </th>
-            </tr>
-        </thead>
-        <tbody>
-        {comps.map((item) => (
+//                 <th scope="col" className="px-6 py-3">
+//                     Name
+//                 </th>
+//                 <th scope="col" className="px-6 py-3">
+//                     Image
+//                 </th>
+//                 <th scope="col" className="px-6 py-3">
+//                     Whatsapp URL
+//                 </th>
+//                 <th scope="col" className="px-6 py-3">
+//                     Gmail URL
+//                 </th>
+//                 <th scope="col" className="px-6 py-3">
+//                     Description
+//                 </th>
+//                 <th scope="col" className="px-6 py-3">
+//                     Charges
+//                 </th>
+//                 <th scope="col" className="px-6 py-3">
+//                     Status
+//                 </th>
+//                 <th scope="col" className="px-6 py-3">
+//                     Action
+//                 </th>
+//             </tr>
+//         </thead>
+//         <tbody>
+//         {comps.map((item) => (
                 
             
-         <tr key={item._id} className="bg-white border-b dark:bg-gray-800 dark:border-gray-700 hover:bg-gray-50 dark:hover:bg-gray-600">
+//          <tr key={item._id} className="bg-white border-b dark:bg-gray-800 dark:border-gray-700 hover:bg-gray-50 dark:hover:bg-gray-600">
                 
-                <th scope="row" className="px-6 overflow-hidden py-4 font-medium text-gray-900 whitespace-nowrap dark:text-white">
-                {item.name}
-                </th>
-                <td className="px-6 overflow-hidden py-4">
-                    <Link href={item.image}><IoMdImage className='ml-1 text-3xl hover:text-primary' /></Link>
-                </td>
-                <td className="px-6 overflow-hidden py-4">
-                <Link href={item.whatsapp}><FaSquareWhatsapp className='ml-3 text-2xl hover:text-secondary' /></Link>
-                </td>
-                <td className="px-6 overflow-hidden py-4">
-                <Link href={item.email}><SiGmail className='ml-5 text-2xl hover:text-info' /></Link>
-                </td>
-                <td className="px-6 overflow-hidden py-4">
-                    {item.desc}
-                </td>
-                <td className="px-6 overflow-hidden py-4">
-                    {item.charges}
-                </td>
-                <td className="px-6 overflow-hidden py-4">
-                    {item.name}
-                </td>
-                <td className="flex items-center px-6 py-4">
-                <div>
+//                 <th scope="row" className="px-6 overflow-hidden py-4 font-medium text-gray-900 whitespace-nowrap dark:text-white">
+//                 {item.name}
+//                 </th>
+//                 <td className="px-6 overflow-hidden py-4">
+//                     <Link href={item.image}><IoMdImage className='ml-1 text-3xl hover:text-primary' /></Link>
+//                 </td>
+//                 <td className="px-6 overflow-hidden py-4">
+//                 <Link href={item.whatsapp}><FaSquareWhatsapp className='ml-3 text-2xl hover:text-secondary' /></Link>
+//                 </td>
+//                 <td className="px-6 overflow-hidden py-4">
+//                 <Link href={item.email}><SiGmail className='ml-5 text-2xl hover:text-info' /></Link>
+//                 </td>
+//                 <td className="px-6 overflow-hidden py-4">
+//                     {item.desc}
+//                 </td>
+//                 <td className="px-6 overflow-hidden py-4">
+//                     {item.charges}
+//                 </td>
+//                 <td className="px-6 overflow-hidden py-4">
+//                     {item.name}
+//                 </td>
+//                 <td className="flex items-center px-6 py-4">
+//                 <div>
       
-      <div>
+//       <div>
       
-      <label htmlFor={item.name} className=""><FaEdit className='text-2xl cursor-pointer text-blue-500'/></label>
+//       <label htmlFor={item.name} className=""><FaEdit className='text-2xl cursor-pointer text-blue-500'/></label>
       
 
-{/* Put this part before </body> tag */}
-<input type="checkbox" id={item.name} className="modal-toggle" />
-<div className="modal" role="dialog">
-  <div className="modal-box">
+// {/* Put this part before </body> tag */}
+// <input type="checkbox" id={item.name} className="modal-toggle" />
+// <div className="modal" role="dialog">
+//   <div className="modal-box">
     
-  <EditServ naam={item.name}/>
+//   <EditServ naam={item.name}/>
 
-<div className="modal-action">
-      <label htmlFor={item.name} className="btn btn-outline btn-error">Close!</label>
-    </div>
+// <div className="modal-action">
+//       <label htmlFor={item.name} className="btn btn-outline btn-error">Close!</label>
+//     </div>
     
-  </div>
-</div>
-    </div>
-    </div>
+//   </div>
+// </div>
+//     </div>
+//     </div>
                     
-                    <div>
-      <label htmlFor={item._id} className=""><MdDelete className='text-2xl cursor-pointer text-red-500 ml-4'/></label>
+//                     <div>
+//       <label htmlFor={item._id} className=""><MdDelete className='text-2xl cursor-pointer text-red-500 ml-4'/></label>
 
-{/* Put this part before </body> tag */}
-<input type="checkbox" id={item._id} className="modal-toggle" />
-<div className="modal" role="dialog">
-  <div className="modal-box">
-    <h3 className="text-lg font-bold">Confirmation!!</h3>
-    <p className="py-4">Do You Really want to delete this entry !?!</p>
-    <div className="modal-action">
-    <DelServ naam={item.name}/>
-    <label htmlFor={item._id} className="btn btn-outline btn-error">No!</label>
-    </div>
-  </div>
-</div>
-    </div>
+// {/* Put this part before </body> tag */}
+// <input type="checkbox" id={item._id} className="modal-toggle" />
+// <div className="modal" role="dialog">
+//   <div className="modal-box">
+//     <h3 className="text-lg font-bold">Confirmation!!</h3>
+//     <p className="py-4">Do You Really want to delete this entry !?!</p>
+//     <div className="modal-action">
+//     <DelServ naam={item.name}/>
+//     <label htmlFor={item._id} className="btn btn-outline btn-error">No!</label>
+//     </div>
+//   </div>
+// </div>
+//     </div>
                     
-                </td>
-            </tr>
-            ))}
-        </tbody>
-    </table>
-</div>
+//                 </td>
+//             </tr>
+//             ))}
+//         </tbody>
+//     </table>
+// </div>
 
-   </div>
-</div>
+//    </div>
+// </div>
 
-    </div>
+//     </div>
+<div>hello</div>
   )
 }
 
--- app/admin/tasks/page.tsx
+++ app/admin/tasks/page.tsx
@@ -17,117 +17,118 @@ const Comps = async() => {
     let comps = Data.comps
     console.log(comps) 
   return (
-    <div>
+//     <div>
      
-     <Remfot/>
-     <div className="p-4 sm:ml-64">
-   <div className="p-4 border-2 border-gray-200 border-dashed rounded-lg dark:border-gray-700 mt-14">
-     <p className='text-2xl text-center font-bold'>Admin Competition Section</p>
-     <div className='flex mt-8'>
-        <p className='w-2/3'>Here you an check and control all our competitions <br /> You can add , delete and edit the competitions</p>
-        <AddTask/>
-     </div>
+//      <Remfot/>
+//      <div className="p-4 sm:ml-64">
+//    <div className="p-4 border-2 border-gray-200 border-dashed rounded-lg dark:border-gray-700 mt-14">
+//      <p className='text-2xl text-center font-bold'>Admin Competition Section</p>
+//      <div className='flex mt-8'>
+//         <p className='w-2/3'>Here you an check and control all our competitions <br /> You can add , delete and edit the competitions</p>
+//         <AddTask/>
+//      </div>
 
      
 
-<div className="relative overflow-x-auto shadow-md sm:rounded-lg mt-8">
-    <table className="w-full text-sm text-left rtl:text-right text-gray-500 dark:text-gray-400">
-        <thead className="text-xs text-gray-700 uppercase bg-gray-50 dark:bg-gray-700 dark:text-gray-400">
-            <tr>
+// <div className="relative overflow-x-auto shadow-md sm:rounded-lg mt-8">
+//     <table className="w-full text-sm text-left rtl:text-right text-gray-500 dark:text-gray-400">
+//         <thead className="text-xs text-gray-700 uppercase bg-gray-50 dark:bg-gray-700 dark:text-gray-400">
+//             <tr>
                 
-                <th scope="col" className="px-6 py-3">
-                    Title
-                </th>
-                <th scope="col" className="px-6 py-3">
-                    Desc
-                </th>
-                <th scope="col" className="px-6 py-3">
-                    Start Date
-                </th>
-                <th scope="col" className="px-6 py-3">
-                    Deadline
-                </th>
-                <th scope="col" className="px-6 py-3">
-                    Category
-                </th>
-                <th scope="col" className="px-6 py-3">
-                    Action
-                </th>
-            </tr>
-        </thead>
-        <tbody>
-        {comps.map((item) => (
+//                 <th scope="col" className="px-6 py-3">
+//                     Title
+//                 </th>
+//                 <th scope="col" className="px-6 py-3">
+//                     Desc
+//                 </th>
+//                 <th scope="col" className="px-6 py-3">
+//                     Start Date
+//                 </th>
+//                 <th scope="col" className="px-6 py-3">
+//                     Deadline
+//                 </th>
+//                 <th scope="col" className="px-6 py-3">
+//                     Category
+//                 </th>
+//                 <th scope="col" className="px-6 py-3">
+//                     Action
+//                 </th>
+//             </tr>
+//         </thead>
+//         <tbody>
+//         {comps.map((item) => (
                 
             
-         <tr key={item._id} className="bg-white border-b dark:bg-gray-800 dark:border-gray-700 hover:bg-gray-50 dark:hover:bg-gray-600">
+//          <tr key={item._id} className="bg-white border-b dark:bg-gray-800 dark:border-gray-700 hover:bg-gray-50 dark:hover:bg-gray-600">
                 
-                <th scope="row" className="px-6 overflow-hidden py-4 font-medium text-gray-900 whitespace-nowrap dark:text-white">
-                {item.title}
-                </th>
-                <td className="px-6 overflow-hidden py-4">
-                    {item.desc}
-                </td>
-                <td className="px-6 overflow-hidden py-4">
-                    {item.start}
-                </td>
-                <td className="px-6 overflow-hidden py-4">
-                    {item.end}
-                </td>
-                <td className="px-6 overflow-hidden py-4">
-                    {item.category}
-                </td>
-                <td className="flex items-center px-6 py-4">
-                <div>
+//                 <th scope="row" className="px-6 overflow-hidden py-4 font-medium text-gray-900 whitespace-nowrap dark:text-white">
+//                 {item.title}
+//                 </th>
+//                 <td className="px-6 overflow-hidden py-4">
+//                     {item.desc}
+//                 </td>
+//                 <td className="px-6 overflow-hidden py-4">
+//                     {item.start}
+//                 </td>
+//                 <td className="px-6 overflow-hidden py-4">
+//                     {item.end}
+//                 </td>
+//                 <td className="px-6 overflow-hidden py-4">
+//                     {item.category}
+//                 </td>
+//                 <td className="flex items-center px-6 py-4">
+//                 <div>
       
-      <div>
+//       <div>
       
-      <label htmlFor={item.title} className=""><FaEdit className='text-2xl cursor-pointer text-blue-500'/></label>
+//       <label htmlFor={item.title} className=""><FaEdit className='text-2xl cursor-pointer text-blue-500'/></label>
       
 
-{/* Put this part before </body> tag */}
-<input type="checkbox" id={item.title} className="modal-toggle" />
-<div className="modal" role="dialog">
-  <div className="modal-box">
+// {/* Put this part before </body> tag */}
+// <input type="checkbox" id={item.title} className="modal-toggle" />
+// <div className="modal" role="dialog">
+//   <div className="modal-box">
     
-  <EditTask naam={item.title}/>
+//   <EditTask naam={item.title}/>
 
-<div className="modal-action">
-      <label htmlFor={item.title} className="btn btn-outline btn-error">Close!</label>
-    </div>
+// <div className="modal-action">
+//       <label htmlFor={item.title} className="btn btn-outline btn-error">Close!</label>
+//     </div>
     
-  </div>
-</div>
-    </div>
-    </div>
+//   </div>
+// </div>
+//     </div>
+//     </div>
                     
-                    <div>
-      <label htmlFor={item._id} className=""><MdDelete className='text-2xl cursor-pointer text-red-500 ml-4'/></label>
+//                     <div>
+//       <label htmlFor={item._id} className=""><MdDelete className='text-2xl cursor-pointer text-red-500 ml-4'/></label>
 
-{/* Put this part before </body> tag */}
-<input type="checkbox" id={item._id} className="modal-toggle" />
-<div className="modal" role="dialog">
-  <div className="modal-box">
-    <h3 className="text-lg font-bold">Confirmation!!</h3>
-    <p className="py-4">Do You Really want to delete this entry !?!</p>
-    <div className="modal-action">
-    <DelTask naam={item.title}/>
-    <label htmlFor={item._id} className="btn btn-outline btn-error">No!</label>
-    </div>
-  </div>
-</div>
-    </div>
+// {/* Put this part before </body> tag */}
+// <input type="checkbox" id={item._id} className="modal-toggle" />
+// <div className="modal" role="dialog">
+//   <div className="modal-box">
+//     <h3 className="text-lg font-bold">Confirmation!!</h3>
+//     <p className="py-4">Do You Really want to delete this entry !?!</p>
+//     <div className="modal-action">
+//     <DelTask naam={item.title}/>
+//     <label htmlFor={item._id} className="btn btn-outline btn-error">No!</label>
+//     </div>
+//   </div>
+// </div>
+//     </div>
                     
-                </td>
-            </tr>
-            ))}
-        </tbody>
-    </table>
-</div>
+//                 </td>
+//             </tr>
+//             ))}
+//         </tbody>
+//     </table>
+// </div>
 
-   </div>
-</div>
+//    </div>
+// </div>
 
-    </div>
+//     </div>
+<div>hello</div>
   )
 }
 
