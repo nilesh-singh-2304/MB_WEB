808884abe90936d2bf675d4e33627b14cb056796
nilesh-singh-2304 <shyaam2304@gmail.com>
2024-07-29 20:00:54 +0530

    new

--- app/member/ai/page.tsx
+++ app/member/ai/page.tsx
@@ -3,28 +3,29 @@ import Bblog from './blog'
 
 const Blogs = () => {
   return (
-    <div className='flex sm:ml-64'>
-        <div className='w-3/4 '>
-      <div className="p-4">
-   <div className="p-2 border-2 h-150 shadow-xl overflow-y-scroll border-gray-200 border-dashed rounded-lg dark:border-gray-700 mt-14">
-   <div className="bg-gray-8000 font-[sans-serif] my-4">
-      <div className="max-w-7xl mx-auto">
-        <div className="text-center">
-          <h2 className="text-3xl font-extrabold text-gray-100 inline-block relative after:absolute after:w-4/6 after:h-1 after:left-0 after:right-0 after:-bottom-4 after:mx-auto after:bg-pink-400 after:rounded-full">LATEST BLOGS</h2>
-        </div>
-        <Bblog/>
-      </div>
-    </div>
-   </div>
-</div>
-    </div>
-    <div className='w-1/4'>
-      <div className="p-4">
-   <div className="p-2 border-2 shadow-xl h-150 overflow-y-scroll border-gray-200 border-dashed rounded-lg dark:border-gray-700 mt-14">
-   </div>
-</div>
-    </div>
-    </div>
+//     <div className='flex sm:ml-64'>
+//         <div className='w-3/4 '>
+//       <div className="p-4">
+//    <div className="p-2 border-2 h-150 shadow-xl overflow-y-scroll border-gray-200 border-dashed rounded-lg dark:border-gray-700 mt-14">
+//    <div className="bg-gray-8000 font-[sans-serif] my-4">
+//       <div className="max-w-7xl mx-auto">
+//         <div className="text-center">
+//           <h2 className="text-3xl font-extrabold text-gray-100 inline-block relative after:absolute after:w-4/6 after:h-1 after:left-0 after:right-0 after:-bottom-4 after:mx-auto after:bg-pink-400 after:rounded-full">LATEST BLOGS</h2>
+//         </div>
+//         <Bblog/>
+//       </div>
+//     </div>
+//    </div>
+// </div>
+//     </div>
+//     <div className='w-1/4'>
+//       <div className="p-4">
+//    <div className="p-2 border-2 shadow-xl h-150 overflow-y-scroll border-gray-200 border-dashed rounded-lg dark:border-gray-700 mt-14">
+//    </div>
+// </div>
+//     </div>
+//     </div>
+<div>hello</div>
   )
 }
 
