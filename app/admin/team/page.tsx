576a196b63d8d155662067f216a3c62c0857449d
nilesh-singh-2304 <shyaam2304@gmail.com>
2024-07-29 12:07:26 +0530

    new commit

--- app/admin/team/page.tsx
+++ app/admin/team/page.tsx
@@ -13,135 +13,136 @@ import { IoSend } from "react-icons/io5";
 import { MdDelete } from "react-icons/md";
 import { FaEdit } from "react-icons/fa";
 const Comps = async() => {
-    let coms = await axios.get(`${process.env.NEXT_PUBLIC_HOST}/api/member`)
-    let Data = coms.data
-    let comps = Data.comps
-    console.log(comps)
+    // let coms = await axios.get(`${process.env.NEXT_PUBLIC_HOST}/api/member`)
+    // let Data = coms.data
+    // let comps = Data.comps
+    // console.log(comps)
   return (
-    <div>
+//     <div>
      
-     <Remfot/>
-     <div className="p-4 sm:ml-64">
-   <div className="p-4 border-2 border-gray-200 border-dashed rounded-lg dark:border-gray-700 mt-14">
-     <p className='text-2xl text-center font-bold'>Admin Competition Section</p>
-     <div className='flex mt-8'>
-        <p className='w-2/3'>Here you an check and control all our competitions <br /> You can add , delete and edit the competitions</p>
-        <AddMem/>
-     </div>
+//      <Remfot/>
+//      <div className="p-4 sm:ml-64">
+//    <div className="p-4 border-2 border-gray-200 border-dashed rounded-lg dark:border-gray-700 mt-14">
+//      <p className='text-2xl text-center font-bold'>Admin Competition Section</p>
+//      <div className='flex mt-8'>
+//         <p className='w-2/3'>Here you an check and control all our competitions <br /> You can add , delete and edit the competitions</p>
+//         <AddMem/>
+//      </div>
 
      
 
-<div className="relative overflow-x-auto shadow-md sm:rounded-lg mt-8">
-    <table className="w-full text-sm text-left rtl:text-right text-gray-500 dark:text-gray-400">
-        <thead className="text-xs text-gray-700 uppercase bg-gray-50 dark:bg-gray-700 dark:text-gray-400">
-            <tr>
+// <div className="relative overflow-x-auto shadow-md sm:rounded-lg mt-8">
+//     <table className="w-full text-sm text-left rtl:text-right text-gray-500 dark:text-gray-400">
+//         <thead className="text-xs text-gray-700 uppercase bg-gray-50 dark:bg-gray-700 dark:text-gray-400">
+//             <tr>
                 
-                <th scope="col" className="px-6 py-3">
-                    Name
-                </th>
-                <th scope="col" className="px-6 py-3">
-                    Branch
-                </th>
-                <th scope="col" className="px-6 py-3">
-                    Batch
-                </th>
-                <th scope="col" className="px-6 py-3">
-                    Role
-                </th>
-                <th scope="col" className="px-6 py-3">
-                    Email
-                </th>
-                <th scope="col" className="px-6 py-3">
-                    Phone
-                </th>
-                <th scope="col" className="px-6 py-3">
-                    MB-Id
-                </th>
-                <th scope="col" className="px-6 py-3">
-                    Action
-                </th>
-            </tr>
-        </thead>
-        <tbody>
-        {comps.map((item) => (
+//                 <th scope="col" className="px-6 py-3">
+//                     Name
+//                 </th>
+//                 <th scope="col" className="px-6 py-3">
+//                     Branch
+//                 </th>
+//                 <th scope="col" className="px-6 py-3">
+//                     Batch
+//                 </th>
+//                 <th scope="col" className="px-6 py-3">
+//                     Role
+//                 </th>
+//                 <th scope="col" className="px-6 py-3">
+//                     Email
+//                 </th>
+//                 <th scope="col" className="px-6 py-3">
+//                     Phone
+//                 </th>
+//                 <th scope="col" className="px-6 py-3">
+//                     MB-Id
+//                 </th>
+//                 <th scope="col" className="px-6 py-3">
+//                     Action
+//                 </th>
+//             </tr>
+//         </thead>
+//         <tbody>
+//         {comps.map((item) => (
                 
             
-         <tr key={item._id} className="bg-white border-b dark:bg-gray-800 dark:border-gray-700 hover:bg-gray-50 dark:hover:bg-gray-600">
+//          <tr key={item._id} className="bg-white border-b dark:bg-gray-800 dark:border-gray-700 hover:bg-gray-50 dark:hover:bg-gray-600">
                 
-                <th scope="row" className="px-6 overflow-hidden py-4 font-medium text-gray-900 whitespace-nowrap dark:text-white">
-                {item.name}
-                </th>
-                <td className="px-6 overflow-hidden py-4">
-                    {item.branch}
-                </td>
-                <td className="px-6 overflow-hidden py-4">
-                {item.batch}
-                </td>
-                <td className="px-6 overflow-hidden py-4">
-                {item.role}
-                </td>
-                <td className="px-6 overflow-hidden py-4">
-                {item.email}
-                </td>
-                <td className="px-6 overflow-hidden py-4">
-                    {item.phone} 
-                </td>
-                <td className="px-6 overflow-hidden py-4">
-                    {item.mbid}
-                </td>
-                <td className="flex items-center px-6 py-4">
-                <div>
+//                 <th scope="row" className="px-6 overflow-hidden py-4 font-medium text-gray-900 whitespace-nowrap dark:text-white">
+//                 {item.name}
+//                 </th>
+//                 <td className="px-6 overflow-hidden py-4">
+//                     {item.branch}
+//                 </td>
+//                 <td className="px-6 overflow-hidden py-4">
+//                 {item.batch}
+//                 </td>
+//                 <td className="px-6 overflow-hidden py-4">
+//                 {item.role}
+//                 </td>
+//                 <td className="px-6 overflow-hidden py-4">
+//                 {item.email}
+//                 </td>
+//                 <td className="px-6 overflow-hidden py-4">
+//                     {item.phone} 
+//                 </td>
+//                 <td className="px-6 overflow-hidden py-4">
+//                     {item.mbid}
+//                 </td>
+//                 <td className="flex items-center px-6 py-4">
+//                 <div>
       
-      <div>
+//       <div>
       
-      <label htmlFor={item.mbpass} className=""><FaEdit className='text-2xl cursor-pointer text-blue-500'/></label>
+//       <label htmlFor={item.mbpass} className=""><FaEdit className='text-2xl cursor-pointer text-blue-500'/></label>
       
 
-{/* Put this part before </body> tag */}
-<input type="checkbox" id={item.mbpass} className="modal-toggle" />
-<div className="modal" role="dialog">
-  <div className="modal-box">
+// {/* Put this part before </body> tag */}
+// <input type="checkbox" id={item.mbpass} className="modal-toggle" />
+// <div className="modal" role="dialog">
+//   <div className="modal-box">
     
-  <EditMem mbid={item.mbid}/>
+//   <EditMem mbid={item.mbid}/>
 
-<div className="modal-action">
-      <label htmlFor={item.mbpass} className="btn btn-outline btn-error">Close!</label>
-    </div>
+// <div className="modal-action">
+//       <label htmlFor={item.mbpass} className="btn btn-outline btn-error">Close!</label>
+//     </div>
     
-  </div>
-</div>
-    </div>
-    </div>
+//   </div>
+// </div>
+//     </div>
+//     </div>
                     
-                    <div>
-      <label htmlFor={item.mbid} className=""><MdDelete className='text-2xl cursor-pointer text-red-500 ml-4'/></label>
+//                     <div>
+//       <label htmlFor={item.mbid} className=""><MdDelete className='text-2xl cursor-pointer text-red-500 ml-4'/></label>
 
-{/* Put this part before </body> tag */}
-<input type="checkbox" id={item.mbid} className="modal-toggle" />
-<div className="modal" role="dialog">
-  <div className="modal-box">
-    <h3 className="text-lg font-bold">Confirmation!!</h3>
-    <p className="py-4">Do You Really want to delete this entry !?!</p>
-    <div className="modal-action">
-    <DelMem mbid={item.mbid}/>
-    <label htmlFor={item.mbid} className="btn btn-outline btn-error">No!</label>
-    </div>
-  </div>
-</div>
+// {/* Put this part before </body> tag */}
+// <input type="checkbox" id={item.mbid} className="modal-toggle" />
+// <div className="modal" role="dialog">
+//   <div className="modal-box">
+//     <h3 className="text-lg font-bold">Confirmation!!</h3>
+//     <p className="py-4">Do You Really want to delete this entry !?!</p>
+//     <div className="modal-action">
+//     <DelMem mbid={item.mbid}/>
+//     <label htmlFor={item.mbid} className="btn btn-outline btn-error">No!</label>
+//     </div>
+//   </div>
+// </div>
 
-    </div>
+//     </div>
                     
-                </td>
-            </tr>
-            ))}
-        </tbody>
-    </table>
-</div>
+//                 </td>
+//             </tr>
+//             ))}
+//         </tbody>
+//     </table>
+// </div>
 
-   </div>
-</div>
+//    </div>
+// </div>
 
-    </div>
+//     </div>
+<div>hello</div>
   )
 }
 
--- app/member/tasks/page.tsx
+++ app/member/tasks/page.tsx
@@ -3,40 +3,41 @@ import { MdOutlineAddTask } from "react-icons/md";
 import axios from 'axios'
 
 const Tasks = async() => {
-  let tasks = await axios.get(`${process.env.NEXT_PUBLIC_HOST}/api/task`)
-    let Data = tasks.data
-    let task = Data.comps
-    console.log(task)
+  // let tasks = await axios.get(`${process.env.NEXT_PUBLIC_HOST}/api/task`)
+  //   let Data = tasks.data
+  //   let task = Data.comps
+  //   console.log(task)
   return (
-    <div>
-      <div className='sm:ml-64 flex'>
-      <div className="p-4 w-full">
-   <div className="p-4 border-2 h-150 overflow-y-scroll border-gray-200 border-dashed rounded-lg dark:border-gray-700 mt-14">
-   <div className="bg-transparent px-4 py-12 font-[sans-serif]">
-      <div className="max-w-6xl mx-auto">
-        <h2 className="text-gray-50 sm:text-4xl text-2xl font-extrabold text-center mb-16">Tasks of the Week</h2>
-        <div className="grid lg:grid-cols-3 md:grid-cols-2 max-md:max-w-lg mx-auto gap-8">
-          {task.map((item) => (
-            <div key={item._id} className="p-6 cursor-pointer flex gap-6 bg-opacity-25 bg-[url('https://e0.pxfuel.com/wallpapers/670/19/desktop-wallpaper-ben-10-symbols-ben-10-omniverse-omnitrix.jpg')] rounded-lg shadow-lg hover:shadow-2xl hover:scale-105 transition-all">
-            <MdOutlineAddTask className='text-6xl'/>
-            <div>
-              <h3 className="text-gray-50 text-2xl font-extrabold mb-3">{item.title}</h3>
-              <p className="text-gray-200 text-sm">{item.desc}</p>
-              <div className='mt-3'>
-              <p className='text-gray-200 text-xs'>Task Started : {item.start}</p>
-              <p className='text-gray-200 text-xs'>Deadline : {item.end}</p>
-              </div>
-            </div>
-          </div>
-          ))}
+//     <div>
+//       <div className='sm:ml-64 flex'>
+//       <div className="p-4 w-full">
+//    <div className="p-4 border-2 h-150 overflow-y-scroll border-gray-200 border-dashed rounded-lg dark:border-gray-700 mt-14">
+//    <div className="bg-transparent px-4 py-12 font-[sans-serif]">
+//       <div className="max-w-6xl mx-auto">
+//         <h2 className="text-gray-50 sm:text-4xl text-2xl font-extrabold text-center mb-16">Tasks of the Week</h2>
+//         <div className="grid lg:grid-cols-3 md:grid-cols-2 max-md:max-w-lg mx-auto gap-8">
+//           {task.map((item) => (
+//             <div key={item._id} className="p-6 cursor-pointer flex gap-6 bg-opacity-25 bg-[url('https://e0.pxfuel.com/wallpapers/670/19/desktop-wallpaper-ben-10-symbols-ben-10-omniverse-omnitrix.jpg')] rounded-lg shadow-lg hover:shadow-2xl hover:scale-105 transition-all">
+//             <MdOutlineAddTask className='text-6xl'/>
+//             <div>
+//               <h3 className="text-gray-50 text-2xl font-extrabold mb-3">{item.title}</h3>
+//               <p className="text-gray-200 text-sm">{item.desc}</p>
+//               <div className='mt-3'>
+//               <p className='text-gray-200 text-xs'>Task Started : {item.start}</p>
+//               <p className='text-gray-200 text-xs'>Deadline : {item.end}</p>
+//               </div>
+//             </div>
+//           </div>
+//           ))}
           
-        </div>
-      </div>
-    </div>
-   </div>
-</div>
-    </div>
-    </div>
+//         </div>
+//       </div>
+//     </div>
+//    </div>
+// </div>
+//     </div>
+//     </div>
+<div>hello</div>
   )
 }
 
